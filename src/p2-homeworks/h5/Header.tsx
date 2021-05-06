c11838c7efc671fe0ac4b48b7d1ea323d69aca3d
vsmaliakou <vsmaliakou@yandex.by>
2021-05-06 10:40:12 +0300

    HW5

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import s from './Header.module.css'
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={s.routes}>
+            <div className={s.menu}>
+                <NavLink to="/pre-junior" activeClassName={s.active}>PreJunior</NavLink>
+                <NavLink to="/junior" activeClassName={s.active}>Junior</NavLink>
+            </div>
+            <div className={s.btn}>Menu</div>
         </div>
     );
 }
--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import {Switch, Redirect, Route} from "react-router-dom";
+import PreJunior from "./pages/PreJunior";
+import Error404 from "./pages/Error404";
 
 export const PATH = {
     PRE_JUNIOR: "/pre-junior",
+    JUNIOR: "/junior",
+    ERROR: "/404"
     // add paths
 }
 
@@ -9,19 +14,21 @@ function Routes() {
     return (
         <div>
             {/*Switch выбирает первый подходящий роут*/}
-            {/*<Switch>*/}
+            <Switch>
 
-            {/*в начале мы попадаем на страницу "/" и переходим сразу на страницу PRE_JUNIOR*/}
-            {/*exact нужен чтоб указать полное совподение (что после "/" ничего не будет)*/}
-            {/*<Route path={"/"} exact render={() => <Redirect to={PRE_JUNIOR}/>}/>*/}
+                {/*в начале мы попадаем на страницу "/" и переходим сразу на страницу PRE_JUNIOR*/}
+                {/*exact нужен чтоб указать полное совподение (что после "/" ничего не будет)*/}
+                <Route path={"/"} exact render={() => <Redirect to={PATH.PRE_JUNIOR}/>}/>
 
-            {/*<Route path={PRE_JUNIOR} render={() => <PreJunior/>}/>*/}
+                <Route path={PATH.PRE_JUNIOR} render={() => <PreJunior/>}/>
+                <Route path={PATH.JUNIOR} render={() => "Sorry, there is nothing here yet"}/>
                 // add routes
 
-            {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
-            {/*<Route render={() => <Error404/>}/>*/}
+                {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
+                <Route path={PATH.ERROR} render={() => <Error404/>}/>
+                <Route path={"*"} render={() => <Redirect to={PATH.ERROR}/>}/>
 
-            {/*</Switch>*/}
+            </Switch>
         </div>
     );
 }
