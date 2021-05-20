947d69c14441c5b4a8aa76bf8bd4b4f23abcc3c8
vsmaliakou <vsmaliakou@yandex.by>
2021-05-20 16:57:47 +0300

    HW9

--- src/p2-homeworks/h8/HW8.tsx
+++ src/p2-homeworks/h8/HW8.tsx
@@ -1,26 +1,36 @@
-import React, {useState} from "react";
-import {homeWorkReducer} from "./bll/homeWorkReducer";
-import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
+import React, {useState} from 'react'
+import {homeWorkReducer} from './bll/homeWorkReducer'
+import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+
+export type UserType = {
+    _id: number
+    name: string
+    age: number
+}
 
 const initialPeople = [
-    {_id: 0, name: "Кот", age: 3},
-    {_id: 1, name: "Александр", age: 66},
-    {_id: 2, name: "Коля", age: 16},
-    {_id: 3, name: "Виктор", age: 44},
-    {_id: 4, name: "Дмитрий", age: 40},
-    {_id: 5, name: "Ирина", age: 55},
+    {_id: 0, name: 'Кот', age: 3},
+    {_id: 1, name: 'Александр', age: 66},
+    {_id: 2, name: 'Коля', age: 16},
+    {_id: 3, name: 'Виктор', age: 44},
+    {_id: 4, name: 'Дмитрий', age: 40},
+    {_id: 5, name: 'Ирина', age: 55},
 ]
 
 function HW8() {
-    const [people, setPeople] = useState(initialPeople);
+    const [people, setPeople] = useState<any>(initialPeople) // need to fix any
 
-    const finalPeople = people.map(p => (
+    // need to fix any
+    const finalPeople = people.map((p: UserType) => (
         <div key={p._id}>
-            some name, age
+            <span>{p.name}</span>
+            {p.age}
         </div>
     ))
 
-    const sortUp = () => setPeople(homeWorkReducer(initialPeople, {type: "sort", payload: "up"}))
+    const sortUp = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'}))
+    const sortDown = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'down'}))
+    const chek18 = () => setPeople(homeWorkReducer(initialPeople, {type: 'check', payload: 18}))
 
     return (
         <div>
@@ -28,19 +38,18 @@ function HW8() {
             homeworks 8
 
             {/*should work (должно работать)*/}
-
             {finalPeople}
-            <div><SuperButton onClick={sortUp}>sort up</SuperButton></div>
-            <div>sort down</div>
 
-            check 18
+            <div><SuperButton onClick={sortUp}>sort up</SuperButton></div>
+            <div><SuperButton onClick={sortDown}>sort down</SuperButton></div>
+            <div><SuperButton onClick={chek18}>chek 18</SuperButton></div>
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativePeople/>*/}
             <hr/>
         </div>
-    );
+    )
 }
 
-export default HW8;
+export default HW8
--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -7,25 +7,25 @@ function Clock() {
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearInterval(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime = date?.toLocaleTimeString() // fix with date
+    const stringDate = date?.toLocaleDateString() // fix with date
 
     return (
         <div>
